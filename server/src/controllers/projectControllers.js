22770a2826ed8f22103cc72e2cabd6ecdbb38f9a
Kshitiz Agrawal <kshitizagrawal@outlook.com>
2022-06-06 21:09:53 +0530

    feat✨: create share identifier API

--- server/src/controllers/projectControllers.js
+++ server/src/controllers/projectControllers.js
@@ -1,5 +1,6 @@
 import db from "../db.js";
 import { createProjectDirectoryUtil } from "./fileControllers.js";
+import { generateSlug } from 'random-word-slugs'
 
 const createProjectInDB = (username, projectName, language) => {
     return new Promise((resolve, reject) => {
@@ -8,7 +9,7 @@ const createProjectInDB = (username, projectName, language) => {
             if (err) {
                 reject(err);
             }
-            resolve({username, projectName, language});
+            resolve({ username, projectName, language });
         })
     })
 }
@@ -24,10 +25,81 @@ export const createProject = (req, res) => {
                     return res.send(result)
                 })
                 .catch(error => {
-                    return res.status(400).send(error);
+                    return res.status(400).send(error.code);
                 })
         }).catch(err => {
             console.error(err)
             return res.status(400).send(err)
+        })
+}
+
+const createShareIdentifierUtil = (username, projectname) => {
+    const identifier = generateSlug(5);
+
+    return new Promise((resolve, reject) => {
+
+        db.query("SELECT * FROM projects WHERE username = ? and name = ?;", [username, projectname], (erro, resu) => {
+            if (erro) {
+                console.log(erro)
+                reject(erro.code)
+            }
+
+            if (resu.length == 0) reject("Username & projectname combination does not exist");
+
+            db.query("update projects set share = ?  WHERE username = ? and name = ?;", [identifier, username, projectname], (err, result) => {
+                if (err) {
+                    console.log(err)
+                    reject(err.code)
+                }
+                console.log(result)
+                resolve({ username, projectname, identifier })
+            })
+        })
+
+    })
+}
+
+export const createShareIdentifier = (req, res) => {
+    const { username, projectname } = req.body;
+    createShareIdentifierUtil(username, projectname)
+        .then(response => {
+            return res.send(response)
+        }).catch(error => {
+            return res.status(400).send(error)
+        })
+}
+
+const getShareIdentifierUtil = (username, projectname) => {
+    return new Promise((resolve, reject) => {
+        db.query("SELECT * FROM projects WHERE username = ? and name = ?;", [username, projectname], (erro, resu) => {
+            if (erro) {
+                console.log(erro)
+                reject(erro.code)
+            }
+
+            console.log(resu)
+            if (resu.length === 0) reject("Username & projectname combination does not exist");
+
+            db.query("SELECT share from projects WHERE username = ? and name = ?;", [username, projectname], (error, result) => {
+                if (error) {
+                    console.log(error);
+                    reject(erro.code);
+                }
+                resolve(result[0]);
+            })
+        })
+    })
+}
+
+export const getShareIdentifier = (req, res) => {
+
+    const { username, projectname } = req.query;
+
+    getShareIdentifierUtil(username, projectname)
+        .then(response => {
+            return res.send(response);
+        })
+        .catch(error => {
+            return res.status(400).send(error);
         })
 }
--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,11 +1,19 @@
 import { Router } from 'express'
-import { createProject } from '../controllers/projectControllers.js';
+import { createProject, createShareIdentifier, getShareIdentifier } from '../controllers/projectControllers.js';
 import { validateToken } from '../middlewares/authMiddleWare.js';
 
 const router = Router();
 
 router.post('/create', validateToken, (req, res) => {
     createProject(req, res);
+})
+
+router.post('/share/create', validateToken, (req, res) => {
+    createShareIdentifier(req, res);
+})
+
+router.get('/share/get', validateToken, (req, res) => {
+    getShareIdentifier(req, res);
 })
 
 export default router;

fb59a63302d18ecae512907822fbd940758a822a
Kshitiz <kshitizagrawal@outlook.com>
2022-06-24 04:21:48 +0530

    fix🐛: check login middleware was giving 200 on not logged in

--- server/src/middlewares/authMiddleWare.js
+++ server/src/middlewares/authMiddleWare.js
@@ -3,7 +3,7 @@ import jsonwebtoken from "jsonwebtoken"
 export const validateToken = (req, res, next) => {
     const accessToken = req.header("accessToken")
 
-    if (!accessToken) return res.json({ error: "User not logged in" })
+    if (!accessToken) return res.status(400).json({ error: "User not logged in" })
 
     try {
         const isValidToken = jsonwebtoken.verify(accessToken, process.env.jsonwebtokensecret)

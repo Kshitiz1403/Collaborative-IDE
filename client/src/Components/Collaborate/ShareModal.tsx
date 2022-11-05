230964ef5e56fa984fb51e210272c634e872dd65
Kshitiz <kshitizagrawal@outlook.com>
2022-11-05 20:26:53 +0530

    Revert "fix🐛: clipboard won't work on http"
    This reverts commit 3311605f8036636f5f955f3d27e5f5995422f0d6.

--- client/src/Components/Collaborate/ShareModal.tsx
+++ client/src/Components/Collaborate/ShareModal.tsx
@@ -1,72 +1,63 @@
-import React, { useEffect, useRef, useState } from 'react';
-import useProjectService from '../../api/projectService';
-import Snacker from '../Snacker/Snacker';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import React, { useEffect, useState } from 'react'
+import useProjectService from '../../api/projectService'
+import Snacker from '../Snacker/Snacker'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal'
+import TextField from '@mui/material/TextField'
 
 const ShareModal = ({ open, setOpen }) => {
-   const [inviteLink, setInviteLink] = useState('');
-   const [invitationAlert, setInvitationAlert] = useState(false);
+    const [inviteLink, setInviteLink] = useState('')
+    const [invitationAlert, setInvitationAlert] = useState(false)
 
-   const { addOrGetSlug } = useProjectService();
+    const {addOrGetSlug} = useProjectService()
 
-   const copyInviteLink = () => {
-      setInvitationAlert(true);
-   };
+    const copyInviteLink = () => {
+        navigator.clipboard.writeText(inviteLink).then(() => setInvitationAlert(true))
+    }
 
-   useEffect(() => {
-      let domain = new URL(window.location.href).host;
+    useEffect(() => {
+        let domain = new URL(window.location.href).host
 
-      if (open) {
-         (async () => {
-            const slug = await addOrGetSlug();
-            setInviteLink(`${domain}/join/${slug}`);
-         })();
-      }
-   }, [open]);
+        if (open) {
+            (async()=>{
+                const slug = await addOrGetSlug()
+                setInviteLink(`${domain}/join/${slug}`)
+            })()
+        }
+    }, [open])
 
-   return (
-      <>
-         <Snacker
-            message={'Invitation URL copied'}
-            open={invitationAlert}
-            severity="success"
-            autoHideDuration={3000}
-            onClose={() => setInvitationAlert(false)}
-         />
-         <Modal open={open} onClose={() => setOpen(false)}>
-            <Box
-               sx={{
-                  position: 'absolute',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
-                  width: 'fit-content',
-                  bgcolor: 'whitesmoke',
-                  borderRadius: 2,
-                  boxShadow: 24,
-                  p: 2,
-                  height: 100,
-                  display: 'flex',
-                  justifyContent: 'center',
-               }}
-            >
-               <div>
-                  <div>Invite friends with a join link</div>
-                  <div style={{ display: 'flex', alignItems: 'center', marginTop: 20 }}>
-                     <TextField size="small" disabled value={inviteLink} />
-                     <CopyToClipboard onCopy={copyInviteLink} text={inviteLink}>
-                        <Button size="small">Copy</Button>
-                     </CopyToClipboard>
-                  </div>
-               </div>
-            </Box>
-         </Modal>
-      </>
-   );
-};
 
-export default ShareModal;
+    return (
+        <>
+            <Snacker message={"Invitation URL copied"} open={invitationAlert} severity='success' autoHideDuration={3000} onClose={() => setInvitationAlert(false)} />
+            <Modal open={open} onClose={() => setOpen(false)}>
+                <Box sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 'fit-content',
+                    bgcolor: 'whitesmoke',
+                    borderRadius: 2,
+                    boxShadow: 24,
+                    p: 2,
+                    height: 100,
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}>
+                    <div>
+                        <div>Invite friends with a join link</div>
+                        <div style={{ display: 'flex', alignItems: 'center', marginTop: 20 }}>
+                            <TextField size='small' disabled value={inviteLink} />
+                            <Button size='small' onClick={copyInviteLink}>Copy</Button>
+                        </div>
+                    </div>
+
+                </Box>
+            </Modal>
+        </>
+    )
+}
+
+export default ShareModal

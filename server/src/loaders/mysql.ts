bbf151e40e6ca2b1f4fd5df6d77f3038b7f87ed6
Kshitiz <kshitizagrawal@outlook.com>
2023-10-23 01:17:32 +0530

    fix: add port for sql connection

--- server/src/loaders/mysql.ts
+++ server/src/loaders/mysql.ts
@@ -4,6 +4,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize(config.databaseName, config.databaseUser, config.databasePassword, {
   host: config.databaseHost,
   dialect: 'mysql',
+  port:3306
 });
 
 export default sequelize;
